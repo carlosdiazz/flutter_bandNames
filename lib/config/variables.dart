8651dac29b2f71574f458caef0fe74f33ca5dce0
Carlos Diaz <c.diazadriann@gmail.com>
2023-04-22 17:50:57 -0400

    Prod

--- lib/config/variables.dart
+++ lib/config/variables.dart
@@ -5,11 +5,11 @@ class Variables {
   //static String apiUrl = "http://192.168.0.135:4000/api";
   //static String socketUrl = "http://192.168.0.135:4000";
 
-  static String apiUrl = "http://192.168.137.108:4000/api";
-  static String socketUrl = "http://192.168.137.108:4000";
+  //static String apiUrl = "http://192.168.137.108:4000/api";
+  //static String socketUrl = "http://192.168.137.108:4000";
 
-  //static String apiUrl =
-  //    "https://nestpruebasocket-production.up.railway.app/api";
-  //static String socketUrl =
-  //    "https://nestpruebasocket-production.up.railway.app/";
+  static String apiUrl =
+      "https://nestpruebasocket-production.up.railway.app/api";
+  static String socketUrl =
+      "https://nestpruebasocket-production.up.railway.app/";
 }
